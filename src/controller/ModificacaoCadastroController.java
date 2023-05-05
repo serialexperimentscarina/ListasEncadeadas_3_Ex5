0c6dbb49e80c872df3190566f7d1754e80b49d64
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-05-05 17:50:32 -0300

    feat: ModificacaoCadastroController (WIP)

---
+++ src/controller/ModificacaoCadastroController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.File;
+//import br.com.serialexperimentscarina.listaobject.ListaObject;
+
+public class ModificacaoCadastroController {
+	
+//	private void novoArquivo(ListaObject l, String nomeArquivo) {
+//		File arquivo = new File("C:\\TEMP", nomeArquivo);
+//		StringBuffer strBuf = new StringBuffer();
+//		
+//		int tamanho = l.size();
+//		for (int i = 0; i < tamanho; i++) {
+//			
+//		}
+//	}
+	
+	public void novoCadastro(int idadeMin, int idadeMax, double limiteCredito) {
+		
+	}
+
+}

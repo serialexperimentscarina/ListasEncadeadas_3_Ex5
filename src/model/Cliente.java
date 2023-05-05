e8878eec9946ee0507290734e9656200e3f40b37
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-05-05 17:33:58 -0300

    Initial commit

---
+++ src/model/Cliente.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Cliente {
+	
+	public String cpf;
+	public String nome;
+	public int idade;
+	public double limiteCredito;
+	
+	public Cliente() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return ("[Cliente " + nome + ", " + idade + " anos, CPF: " + cpf + ", limite de crédito: R$ " + String.format("%,.2f", limiteCredito) + "]");
+	}
+
+}
